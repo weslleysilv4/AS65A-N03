035bcf7d1f2417f81c3996c6c6eee8dd521f6679
Victor Gabriel <victor_tiss@outlook.com>
2025-06-24 14:43:47 -0300

    refactor(categories): reorganize structure of routes and controllers for category management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
-import express from "express"
-import routes from "./routes/routes"
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 export default app;
--- backend/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import categoryRoutes from "./categoryRoutes";
-
-const router = Router();
-
-router.use("/categories", categoryRoutes);
-
-export default router;
