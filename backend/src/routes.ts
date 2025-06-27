1a70e6ada892b91f6a3ee6e067c37321cd7feb72
Victor Gabriel <victor_tiss@outlook.com>
2025-06-27 18:10:19 -0300

    feat(middleware): add error handling middleware and integrate authentication routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import categoryRoutes from './modules/categories/categories.router';
+import authRoutes from './modules/auth/auth.router';
 
 const router = Router();
 
 router.use('/categories', categoryRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
